a661898e09835b054ca295564dbd46045f779dcd
Khausta <anna.a.khaustova@gmail.com>
2024-05-29 21:59:43 +0300

    first commit

---
+++ dist/js/utils.js
@@ -0,0 +1,10 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.sayHi = sayHi;
+function sayHi(name) {
+  console.log('hi! ' + name);
+}
+//# sourceMappingURL=utils.js.map
---
+++ src/scripts/main.js
@@ -0,0 +1,14 @@
+
+// const headerBlock = {
+//     headInner: document.querySelector('.head-inner'),
+//     header: document.querySelector('.header'),
+    
+    
+// }
+
+// function addTopPosition() {
+//     headerBlock.header.style.top = headerBlock.headInner.scrollHeight + 'px';
+// }
+
+// addTopPosition();
+
