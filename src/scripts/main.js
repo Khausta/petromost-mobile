1dea2fd2c9cd029f7e22fd776907dc9e3fb5c931
Khausta <anna.a.khaustova@gmail.com>
2024-06-02 11:34:12 +0300

    category is ready

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,14 +1,21 @@
-
-// const headerBlock = {
-//     headInner: document.querySelector('.head-inner'),
-//     header: document.querySelector('.header'),
-    
-    
-// }
-
-// function addTopPosition() {
-//     headerBlock.header.style.top = headerBlock.headInner.scrollHeight + 'px';
-// }
-
-// addTopPosition();
-
+const chanceSwiper = new Swiper('.chance-swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.cahance-button-next',
+    //   prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
