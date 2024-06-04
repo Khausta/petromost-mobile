42752b79c1c6618beaa883c856360f64f9651df7
Khausta <anna.a.khaustova@gmail.com>
2024-06-04 23:50:55 +0300

    search popup is ready

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,4 +23,25 @@ var chanceSwiper = new Swiper('.chance-swiper', {
     nextEl: '.chance-button-next'
   }
 });
+
+// popups 
+var searchInput = document.querySelector('.__js-searchInput');
+var searchOverlay = document.querySelector('#search-overlay');
+var header = document.querySelector('.header');
+var logo = document.querySelector('.__js-logo');
+searchInput.addEventListener('focus', function () {
+  openPopup();
+});
+function openPopup() {
+  searchOverlay.classList.add('__js-active');
+  if (!header.classList.contains('__js-fixed')) {
+    header.classList.add('__js-fixed');
+  }
+  if (document.body.style.overflow !== 'hidden') {
+    document.body.style.overflow = 'hidden';
+  }
+  logo.classList.add('__js-logo_none');
+  searchInput.classList.add('__js-searchInput_active');
+  searchInput.parentElement.classList.add('__js-headerSearch_active');
+}
 //# sourceMappingURL=main.js.map
--- src/scripts/main.js
+++ src/scripts/main.js
@@ -24,3 +24,30 @@ const chanceSwiper = new Swiper('.chance-swiper', {
       nextEl: '.chance-button-next',
     },
   });
+
+
+// popups 
+const searchInput = document.querySelector('.__js-searchInput');
+const searchOverlay = document.querySelector('#search-overlay');
+const header = document.querySelector('.header');
+const logo = document.querySelector('.__js-logo');
+
+searchInput.addEventListener('focus', () => {
+  openPopup();
+})
+
+function openPopup () {
+  searchOverlay.classList.add('__js-active');
+
+  if (!header.classList.contains('__js-fixed')) {
+    header.classList.add('__js-fixed');
+  }
+
+  if (document.body.style.overflow !== 'hidden') {
+    document.body.style.overflow = 'hidden';
+  } 
+
+  logo.classList.add('__js-logo_none');
+  searchInput.classList.add('__js-searchInput_active');
+  searchInput.parentElement.classList.add('__js-headerSearch_active');
+}
