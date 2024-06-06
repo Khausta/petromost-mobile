da0a30641de29820eb7c52d8e62071f245f4ba2f
Khausta <anna.a.khaustova@gmail.com>
2024-06-06 23:14:51 +0300

    search, menu

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,25 +23,91 @@ var chanceSwiper = new Swiper('.chance-swiper', {
     nextEl: '.chance-button-next'
   }
 });
+var headerElements = {
+  header: document.querySelector('.header'),
+  logo: document.querySelector('.__js-logo'),
+  openMenuOrCloseAllBtn: document.querySelector('.menu-btn')
+};
+var popups = {
+  search: {
+    overlay: document.querySelector('#search-overlay'),
+    input: document.querySelector('.__js-searchInput'),
+    open: function open() {
+      popups.openedPopups.search = true;
+      popups.search.overlay.classList.add('__js-active');
+    },
+    close: function close() {
+      popups.search.overlay.classList.remove('__js-active');
+    }
+  },
+  menu: {
+    overlay: document.querySelector('#menu-overlay'),
+    menuContent: document.querySelector('#menu-popup')
+  },
+  openedPopups: {
+    search: false,
+    menu: false
+  }
+};
 
 // popups 
-var searchInput = document.querySelector('.__js-searchInput');
-var searchOverlay = document.querySelector('#search-overlay');
-var header = document.querySelector('.header');
-var logo = document.querySelector('.__js-logo');
-searchInput.addEventListener('focus', function () {
-  openPopup();
+// const searchInput = document.querySelector('.__js-searchInput');
+// const searchOverlay = document.querySelector('#search-overlay');
+// const header = document.querySelector('.header');
+// const logo = document.querySelector('.__js-logo');
+// const menuBtn = document.querySelector('.menu-btn');
+
+popups.search.input.addEventListener('focus', function () {
+  openSearchPopup();
 });
-function openPopup() {
-  searchOverlay.classList.add('__js-active');
-  if (!header.classList.contains('__js-fixed')) {
-    header.classList.add('__js-fixed');
+function openSearchPopup() {
+  popups.search.open();
+  if (!headerElements.header.classList.contains('__js-fixed')) {
+    headerElements.header.classList.add('__js-fixed');
   }
   if (document.body.style.overflow !== 'hidden') {
     document.body.style.overflow = 'hidden';
   }
-  logo.classList.add('__js-logo_none');
-  searchInput.classList.add('__js-searchInput_active');
-  searchInput.parentElement.classList.add('__js-headerSearch_active');
+  popups.search.input.classList.add('__js-searchInput_active');
+  popups.search.input.parentElement.classList.add('__js-headerSearch_active');
+  headerElements.openMenuOrCloseAllBtn.classList.add('__js_active');
+  headerElements.logo.classList.add('__js-logo_none');
+  headerElements.openMenuOrCloseAllBtn.addEventListener('click', closePopup);
 }
+function closePopup() {
+  document.body.style.overflow = 'auto';
+  headerElements.header.classList.remove('__js-fixed');
+  headerElements.logo.classList.remove('__js-logo_none');
+  popups.search.input.classList.remove('__js-searchInput_active');
+  popups.search.input.parentElement.classList.remove('__js-headerSearch_active');
+  if (popups.openedPopups.search) {
+    popups.search.close();
+  }
+  if (popups.openedPopups.menu) {
+    popups.menu.overlay.classList.remove('__js-active');
+    // popups.menu.menuContent.classList.remove('__js-active');
+  }
+  headerElements.openMenuOrCloseAllBtn.classList.remove('__js_active');
+  headerElements.openMenuOrCloseAllBtn.removeEventListener('click', closePopup);
+  popups.openedPopups.search = false;
+  popups.openedPopups.menu = false;
+}
+headerElements.openMenuOrCloseAllBtn.addEventListener('click', function () {
+  if (popups.openedPopups.search) {
+    return;
+  }
+  popups.openedPopups.menu = true;
+  if (!headerElements.header.classList.contains('__js-fixed')) {
+    headerElements.header.classList.add('__js-fixed');
+  }
+  if (document.body.style.overflow !== 'hidden') {
+    document.body.style.overflow = 'hidden';
+  }
+  popups.search.input.classList.add('__js-searchInput_active');
+  popups.search.input.parentElement.classList.add('__js-headerSearch_active');
+  popups.menu.overlay.classList.add('__js-active');
+  headerElements.openMenuOrCloseAllBtn.classList.add('__js_active');
+  headerElements.logo.classList.add('__js-logo_none');
+  headerElements.openMenuOrCloseAllBtn.addEventListener('click', closePopup);
+});
 //# sourceMappingURL=main.js.map
