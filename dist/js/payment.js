d3097526cc73d7549689cc8144daff44f8919d2b
Khausta <anna.a.khaustova@gmail.com>
2024-07-07 21:48:30 +0300

    payment, cancel, rating

--- dist/js/payment.js
+++ dist/js/payment.js
@@ -2,19 +2,10 @@
 
 var cvvInput = document.querySelector('#cvv');
 var unhideBtn = document.querySelector('.pay-card-data__to-unhide');
-var cvvString = [];
-var cvvStars = [];
-cvvInput.addEventListener('input', function (e) {
-  console.log(e);
-  cvvString.push(cvvInput.value[cvvInput.value.length - 1]);
-  cvvStars.push('*');
-  cvvInput.value = cvvStars.join('');
-});
 unhideBtn.addEventListener('click', function () {
-  cvvInput.value = cvvString.join('');
+  cvvInput.setAttribute('type', 'text');
   setTimeout(function () {
-    cvvInput.value = cvvStars.join('');
+    cvvInput.setAttribute('type', 'password');
   }, 2000);
 });
-cvvInput.innerText = '***';
 //# sourceMappingURL=payment.js.map
--- src/scripts/payment.js
+++ src/scripts/payment.js
@@ -1,21 +1,10 @@
 const cvvInput = document.querySelector('#cvv');
 const unhideBtn = document.querySelector('.pay-card-data__to-unhide');
-let cvvString = [];
-let cvvStars = [];
-
-cvvInput.addEventListener('input', (e) => {
-    console.log(e);
-    cvvString.push(cvvInput.value[cvvInput.value.length - 1]);
-    cvvStars.push('*');
-    cvvInput.value = cvvStars.join('');
-});
-
 unhideBtn.addEventListener('click', () => {
-    cvvInput.value = cvvString.join('');
+    cvvInput.setAttribute('type', 'text');
     setTimeout(()=> {
-        cvvInput.value = cvvStars.join('');
+        cvvInput.setAttribute('type', 'password');
     }, 2000);
-});
+})
 
-cvvInput.innerText = '***'
 
