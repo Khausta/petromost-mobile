5c4f00acb38042f7a1e18d5db23619e2f9014fea
Khausta <anna.a.khaustova@gmail.com>
2024-08-14 22:28:52 +0300

    sliders

--- dist/js/sliders.js
+++ dist/js/sliders.js
@@ -9,17 +9,54 @@ var bannerSwiper = new Swiper('.banner-swiper', {
     clickable: true
   }
 });
-var storyPopupSwiper = new Swiper('.story-swiper', {
+var closeWeeks = document.querySelector('.close-promotions-week');
+closeWeeks.addEventListener('click', function () {
+  storyPopupSwiper1.slideTo(0);
+});
+var closeCashback = document.querySelector('.close-promotions-cashback');
+closeCashback.addEventListener('click', function () {
+  storyPopupSwiper2.slideTo(0);
+});
+var storyPopupSwiper1 = new Swiper('.story-swiper-1', {
   spaceBetween: '4%',
   slidesPerView: 'auto',
-  loop: true,
   autoplay: {
     delay: 5000
   },
   pagination: {
-    el: ".story-pagination",
+    el: ".story-pagination-1",
     clickable: true
+  },
+  on: {
+    reachBeginning: function reachBeginning() {
+      closeWeeks.click();
+    }
   }
+});
+var storyPopupSwiper2 = new Swiper('.story-swiper-2', {
+  spaceBetween: '4%',
+  slidesPerView: 'auto',
+  autoplay: {
+    delay: 5000
+  },
+  pagination: {
+    el: ".story-pagination-2",
+    clickable: true
+  },
+  on: {
+    reachBeginning: function reachBeginning() {
+      closeCashback.click();
+    }
+  }
+});
+var weeksBtn = document.querySelector('.weeksBtn');
+weeksBtn.addEventListener('click', function () {
+  storyPopupSwiper1.slideTo(0);
+});
+var cashbackBtn = document.querySelector('.cashbackBtn');
+cashbackBtn.addEventListener('click', function () {
+  storyPopupSwiper2.slideTo(0);
+  console.log("cashback is opened");
 });
 var infoSwiper = new Swiper('.info-swiper', {
   spaceBetween: '2%',
--- src/scripts/sliders.js
+++ src/scripts/sliders.js
@@ -8,18 +8,63 @@ const bannerSwiper = new Swiper('.banner-swiper', {
     },
   });
 
-  const storyPopupSwiper = new Swiper('.story-swiper', {
+  const closeWeeks = document.querySelector('.close-promotions-week');
+  closeWeeks.addEventListener('click', () => {
+    storyPopupSwiper1.slideTo(0);
+  })
+
+  const closeCashback = document.querySelector('.close-promotions-cashback');
+  closeCashback.addEventListener('click', () => {
+    storyPopupSwiper2.slideTo(0);
+  })
+
+
+  const storyPopupSwiper1 = new Swiper('.story-swiper-1', {
     spaceBetween: '4%',
     slidesPerView: 'auto',
-    loop: true,
     autoplay: {
       delay: 5000,
     },
     pagination: {
-      el: ".story-pagination",
+      el: ".story-pagination-1",
       clickable: true,
     },
+    on: {
+      reachBeginning: function () {
+        closeWeeks.click();
+      },
+    }
   });
+
+  const storyPopupSwiper2 = new Swiper('.story-swiper-2', {
+    spaceBetween: '4%',
+    slidesPerView: 'auto',
+    autoplay: {
+      delay: 5000,
+    },
+    pagination: {
+      el: ".story-pagination-2",
+      clickable: true,
+    },
+    on: {
+      reachBeginning: function () {
+        closeCashback.click();
+      },
+    }
+  });
+  
+
+  const weeksBtn = document.querySelector('.weeksBtn');
+  weeksBtn.addEventListener('click', () => {
+    storyPopupSwiper1.slideTo(0);
+    })
+
+  const cashbackBtn = document.querySelector('.cashbackBtn');
+  cashbackBtn.addEventListener('click', () => {
+    storyPopupSwiper2.slideTo(0);
+    console.log("cashback is opened");
+    })
+    
   
   const infoSwiper = new Swiper('.info-swiper', {
     spaceBetween: '2%',
